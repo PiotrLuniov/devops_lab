cf24a912e185ec2cc8131e7c472578ed5ae5c25b
Piotr_Luniov <piotr_luniov@epam.com>
2019-06-27 10:57:27 +0300

    Changes

--- pr-stats.py
+++ pr-stats.py
@@ -1,6 +1,5 @@
 from datetime import date, datetime
 import requests
-import json
 import argparse
 
 # Parsing arguments
@@ -8,51 +7,63 @@ parser = argparse.ArgumentParser(add_help=True,
                                  description='Get Pull-request statistics from GitHub')
 
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-parser.add_argument('-user', type=str, help='add username')
-parser.add_argument('-repo',
-                    type=str,
-                    default='https://api.github.com/repos/alenaPy/devops_lab/pulls/42',
-                    help='add pull url')
+
+parser.add_argument('-o', '--owner', help="owner of repository", type=str, default="alenaPy")
+parser.add_argument('-r', '--repo', help="name of repository", type=str, default="devops_lab")
+parser.add_argument('-p', '--pull', help="name of pull request", type=int, default=42)
+
+parser.add_argument('-t', '--token', help="add token for access", type=str)
+
+parser.add_argument('--all', help="output all available parameters", action="store_true")
+
+parser.add_argument('-uo', help='output username,who opened pull request', action="store_true")
+parser.add_argument('-dw', help='output day of week,when opened pull request', action="store_true")
+parser.add_argument('-ho', help='output hour,when opened pull request', action="store_true")
+parser.add_argument('-wo', help='output week,when opened pull request', action="store_true")
+parser.add_argument('-do', help='output quantity of days,when opened pull request',
+                    action="store_true")
 
 args = parser.parse_args()
 
-if not args.user:
-    parser.error('Action requested, add -user')
+repos_url = "https://api.github.com/repos/" + args.owner + "/" + args.repo + "/pulls/" \
+            + str(args.pull)
+token = args.token
 
-if args.user:
-    username = args.user
-    repos_url = args.repo
+# Create request
+headers = {'Authorization': 'token ' + token}
+repos = requests.get(repos_url, headers=headers).json()
 
-    token = '842d1bcfb91f4083e049f075d966aeca51da065f'
+# Get info about creation_PR
+cr_date = repos['created_at']
+dt = cr_date[:10]
+year, month, day = (int(x) for x in dt.split('-'))
+date_opened = date(year, month, day)
 
-    # Create a re-usable session object with the user creds in-built
-    gh_session = requests.Session()
-    gh_session.auth = (username, token)
+# Get necessary info
+UserName = repos['user']['login']
+DayofWeek = date_opened.strftime('%A')
+Week = date_opened.strftime('%U')
+Hour = cr_date[11:13]
 
-    # Get PR info
-    repos = json.loads(gh_session.get(repos_url).text)
+# Get NumberofDays_opened_PR
+previous_date = datetime.strptime(dt, '%Y-%m-%d')
+today = datetime.today()
+Ndays = str((today - previous_date).days)
 
-    # User who opened
-    print('User_opened_PR: ' + repos['user']['login'])
+if not args.token:
+    parser.error('Action requested, add --token')
 
-    # Day of the week opened
-    cr_date = repos['created_at']
-    dt = cr_date[:10]
-    year, month, day = (int(x) for x in dt.split('-'))
-    date_opened = date(year, month, day)
-    DayofWeek = date_opened.strftime('%A')
+if args.all or args.uo:
+    print('User_opened_PR: ' + UserName)
+
+if args.all or args.dw:
     print('DayoftheWeek_opened_PR: ' + DayofWeek)
 
-    # NumberofWeek
-    Week = date_opened.strftime('%U')
+if args.all or args.wo:
     print('Week_opened_PR: ' + Week)
 
-    # Hour of the day opened
-    Hour = cr_date[11:13]
+if args.all or args.ho:
     print('Hour_opened_PR: ' + Hour)
 
-    # Number of days opened
-    previous_date = datetime.strptime(dt, '%Y-%m-%d')
-    today = datetime.today()
-    ndays = (today - previous_date).days
-    print('NumberofDays_opened_PR: ' + str(ndays))
+if args.all or args.do:
+    print('NumberofDays_opened_PR: ' + Ndays)
